433f640ed69f8a709e311f5d27b0ceb91f967ad9
gayannettasinghe <gayannagc@gmail.com>
2021-10-03 15:35:56 +0530

    Initial commit

---
+++ src/main/java/com/example/javapersistencepractice/JavaPersistencePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.javapersistencepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPersistencePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPersistencePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javapersistencepractice/JavaPersistencePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javapersistencepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPersistencePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
